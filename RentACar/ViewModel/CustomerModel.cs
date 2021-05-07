d432b3f23eb181317faa8d9fcdc22486b9390ca3
Root <Root@DESKTOP-89ILC7V>
2021-05-07 14:44:54 +0300

    Add project files.

---
+++ RentACar/ViewModel/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentACar.ViewModel
+{
+    public class CustomerModel
+    {
+        public string customerId { get; set; }
+        public string customerName { get; set; }
+
+        public System.DateTime customerBirthDate { get; set; }
+        public string customerMail { get; set; }
+    }
+}
